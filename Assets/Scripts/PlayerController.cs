b4b7a8ae361aa387a7bd6c8b843997df43a2ccf0
Fred Duarte Jacome <fduarte@bcp.com.pe>
2019-02-06 19:32:37 -0500

    Second Class2 1 Commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,50 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    bool canJump;
 
+    bool canJump;
     // Start is called before the first frame update
     void Start()
     {
-        //gameObject.GetComponent<Transform>().position = new Vector3(5, 0, 0);
-        /*gameObject.transform.position = new Vector3(gameObject.transform.position.x + 5, 
-            gameObject.transform.position.y, gameObject.transform.position.z);*/
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*gameObject.transform.position = new Vector3(gameObject.transform.position.x + 0.1f * Time.deltaTime,
-            gameObject.transform.position.y, gameObject.transform.position.z);*/
-        /*gameObject.transform.position = new Vector3(gameObject.transform.position.x + 50f * Time.deltaTime,
-            gameObject.transform.position.y, gameObject.transform.position.z);*/
-        //gameObject.transform.Translate(50f * Time.deltaTime, 0, 0);
-
         if (Input.GetKey("left")) {
-            gameObject.transform.Translate(-50f * Time.deltaTime, 0, 0);
+            //unity asume que cada pixel equivale un metro
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-1000f * Time.deltaTime, 0));
         }
         if (Input.GetKey("right"))
         {
-            gameObject.transform.Translate(50f * Time.deltaTime, 0, 0);
+            //unity asume que cada pixel equivale un metro
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(1000f * Time.deltaTime, 0));
         }
-        ManageJump();
+
+        if (Input.GetKeyDown("up") && canJump)
+        {
+            canJump = false;
+            //unity asume que cada pixel equivale un metro
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0 , 300f));
+        }
+
     }
 
-    void ManageJump() {
-        if (gameObject.transform.position.y <= 0) { //esta en el suelo
+    private void OnCollisionEnter2D(Collision2D collision) {
+
+        if (collision.transform.tag == "ground") {
             canJump = true;
         }
 
-        if (Input.GetKey("up") && canJump && gameObject.transform.position.y < 10) //puede saltar
-        {
-            //mover hacia arriba
-            gameObject.transform.Translate(0, 50f * Time.deltaTime, 0);
-        }
-        else {
-            canJump = false;
-            if (gameObject.transform.position.y > 0) { //Player en el aire
-                gameObject.transform.Translate(0, -50f * Time.deltaTime, 0);
-            }
-        }
     }
 }
